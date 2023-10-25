eeb6c6c44b355f867fc23bc441a81293c9924f5d
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-10-26 00:09:16 +0200

    create env.example and try to fix frontend deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
 import Dashboard from './Pages/Dashboard'
-import Shop from './Pages/shop/shop'
+// import Shop from './Pages/shop/shop'
 import Cart from './Pages/cart/cart'
 import Navbar from "./components/Navbar"
 import {ShopContextProvider} from './context/context'
@@ -12,6 +12,7 @@ import ProductDetail from './components/ProductDetail'
 import CreateProduct from './Pages/shop/CreateProduct'
 import Success from './Pages/payment/success'
 import Cancel from './Pages/payment/cancel'
+import Shop from './Pages/shop/Shop'
 
 
 function App() {
