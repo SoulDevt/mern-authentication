af7eaaae90053dfcc355cf621f1c0c83476f7226
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-11-09 17:19:56 +0100

    add design to cart and cartItem

--- client/src/Pages/cart/CartItem.jsx
+++ client/src/Pages/cart/CartItem.jsx
@@ -2,22 +2,43 @@ import React, { useContext, useEffect } from 'react'
 import { ShopContext } from '../../context/context'
 import { Link } from 'react-router-dom'
 
-const CartItem = ({product}) => {
+const CartItem = ({ product }) => {
   useEffect(() => {
     console.log("launched");
     console.log("products:" + product)
-  },[])
-const {cartItems, addItemToCart, removeItemToCart, updateItemToCart} = useContext(ShopContext)
+  }, [])
+  const { cartItems, addItemToCart, removeItemToCart, updateItemToCart } = useContext(ShopContext)
   return (
-        <div className="product">
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>{product.price} €</p>
-            <img src={product.img} />
-            <button onClick={() => removeItemToCart(product._id)}> - </button>
-            <input type="text" value={cartItems[product._id]} onChange={(e) => updateItemToCart(Number(e.target.value), product.id)} />
-            <button onClick={() => addItemToCart(product._id)}> + </button>
-        </div>
+    <div className="border border-gray-300 p-4 rounded-md mb-4 flex items-center">
+      <img src={product.imageUrl} alt={product.name} className="w-16 h-16 object-cover mr-4" />
+
+      <div className="flex-grow">
+        <h3 className="text-lg font-semibold">{product.name}</h3>
+        <p className="text-gray-600">{product.description}</p>
+        <p className="text-gray-800 font-semibold">${product.price.toFixed(2)}</p>
+      </div>
+
+      <div className="flex items-center">
+        <button
+          className="text-red-600 font-semibold px-2"
+          onClick={() => removeItemToCart(product._id)}
+        >
+          -
+        </button>
+        <input
+          className="w-8 text-center border border-gray-300 mx-2"
+          type="text"
+          value={cartItems[product._id]}
+          onChange={(e) => updateItemToCart(Number(e.target.value), product._id)}
+        />
+        <button
+          className="text-green-600 font-semibold px-2"
+          onClick={() => addItemToCart(product._id)}
+        >
+          +
+        </button>
+      </div>
+    </div>
   )
 }
 
