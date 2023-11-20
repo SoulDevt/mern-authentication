935d59df35f482f7e243dc82f93895c8929df5a7
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-11-20 17:51:57 +0100

    pagination

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -9,41 +9,53 @@ import Comments from './comments/Comments'
 
 
 const ProductDetail = () => {
-    const {cartItems, addItemToCart, products} = useContext(ShopContext)
+    const { cartItems, addItemToCart, products } = useContext(ShopContext)
     const [product, setProduct] = useState(null)
     const [comment, setComment] = useState('')
     const [allComments, setAllComments] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+
     const userString = localStorage.getItem("user");
     let user;
     if (userString) {
-      user = JSON.parse(userString);
+        user = JSON.parse(userString);
     }
-     const { id } = useParams()
+    const { id } = useParams()
+
+    // const fetchComments = async () => {
+    //     try {
+    //         await axios.get(`${import.meta.env.VITE_API_URL}/products/comments`)
+    //         .then((response) => {
+    //             const getComments = response.data.filter((comment) => comment.productId === id)
+    //             setAllComments(getComments)
+    //             return getComments;
+    //         })
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
     const fetchComments = async () => {
         try {
-            await axios.get(`${import.meta.env.VITE_API_URL}/products/comments`)
-            .then((response) => {
-                const getComments = response.data.filter((comment) => comment.productId === id)
-                setAllComments(getComments)
-                return getComments;
-            })
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}/products/comments?productId=${id}&page=${currentPage}`);
+            setAllComments(response.data);
+            console.log(response.data)
         } catch (error) {
-            console.log(error)
+            console.error(error);
         }
-    }
+    };
 
     useEffect(() => {
-        const fetchProduct = async() => {
+        const fetchProduct = async () => {
             try {
                 await axios.get(`${import.meta.env.VITE_API_URL}/product/${id}`)
-                .then((response) => {
-                    setProduct(response.data);
-                    console.log(response.data)
-                })
-                .catch((error) => {
-                    console.log(error.response.data)
-                })
+                    .then((response) => {
+                        setProduct(response.data);
+                        console.log(response.data)
+                    })
+                    .catch((error) => {
+                        console.log(error.response.data)
+                    })
             } catch (error) {
                 console.log(error)
             }
@@ -53,19 +65,19 @@ const ProductDetail = () => {
         //fetch comments
         fetchComments();
         //end fetch comments
-    },[])
+    }, [id, currentPage])
 
     const handleComment = async () => {
         try {
-            await axios.post(`${import.meta.env.VITE_API_URL}/product/create-comment`, {comment, id, userId: user.id}, {withCredentials: true})
-            .then((response) => {
-                console.log(response);
-                setComment('');
-                fetchComments();
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+            await axios.post(`${import.meta.env.VITE_API_URL}/product/create-comment`, { comment, id, userId: user.id }, { withCredentials: true })
+                .then((response) => {
+                    console.log(response);
+                    setComment('');
+                    fetchComments();
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
         } catch (error) {
             console.log(error)
         }
@@ -73,86 +85,104 @@ const ProductDetail = () => {
 
     const addItemToWishlist = async () => {
         try {
-            await axios.post(`${import.meta.env.VITE_API_URL}/wishlist/add`, {productId: id, userId: user.id}, {withCredentials: true})
-            .then((response) => {
-                console.log(response.data);
-            });
+            await axios.post(`${import.meta.env.VITE_API_URL}/wishlist/add`, { productId: id, userId: user.id }, { withCredentials: true })
+                .then((response) => {
+                    console.log(response.data);
+                });
         } catch (error) {
             console.log(error)
         }
     }
-  return (
-    <div className="max-w-screen-xl mx-auto px-4 py-8">
-      {product && (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="w-full">
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="w-full h-auto rounded-lg"
-            />
-          </div>
-          <div>
-            <h1 className="text-2xl font-semibold mb-4">{product.name}</h1>
-            <p className="text-gray-600 text-lg">{product.description}</p>
-            <p className="text-lg font-semibold text-indigo-600 mt-4">
-              ${product.price}
-            </p>
-            <div className="mt-6">
-              <button
-                className="px-4 py-2 bg-indigo-600 text-white rounded-md mr-4"
-                onClick={() => addItemToCart(id)}
-              >
-                Add to Cart {cartItems[id] > 0 && `(${cartItems[id]})`}
-              </button>
-              <button
-                className="px-4 py-2 bg-yellow-500 text-white rounded-md"
-                onClick={addItemToWishlist}
-              >
-                Add to Wishlist
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
 
-      <div className="mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Comments</h2>
-        <div className="mb-4">
-          <p className="text-lg font-semibold">Leave a comment</p>
-          <textarea
-            className="w-full p-2 border border-gray-300 rounded-md text-sm"
-            cols="30"
-            rows="5"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-          ></textarea>
-          <button
-            className="px-4 py-2 bg-indigo-600 text-white rounded-md ml-2 text-sm"
-            onClick={handleComment}
-          >
-            Submit
-          </button>
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+
+    return (
+        <div className="max-w-screen-xl mx-auto px-4 py-8">
+            {product && (
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div className="w-full">
+                        <img
+                            src={product.imageUrl}
+                            alt={product.name}
+                            className="w-full h-auto rounded-lg"
+                        />
+                    </div>
+                    <div>
+                        <h1 className="text-2xl font-semibold mb-4">{product.name}</h1>
+                        <p className="text-gray-600 text-lg">{product.description}</p>
+                        <p className="text-lg font-semibold text-indigo-600 mt-4">
+                            ${product.price}
+                        </p>
+                        <div className="mt-6">
+                            <button
+                                className="px-4 py-2 bg-indigo-600 text-white rounded-md mr-4"
+                                onClick={() => addItemToCart(id)}
+                            >
+                                Add to Cart {cartItems[id] > 0 && `(${cartItems[id]})`}
+                            </button>
+                            <button
+                                className="px-4 py-2 bg-yellow-500 text-white rounded-md"
+                                onClick={addItemToWishlist}
+                            >
+                                Add to Wishlist
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            <div className="mt-8">
+                <h2 className="text-2xl font-semibold mb-4">Comments of users</h2>
+                <div className="mb-4">
+                    <p className="text-lg font-semibold">Leave a comment</p>
+                    <textarea
+                        className="w-full p-2 border border-gray-300 rounded-md text-sm"
+                        cols="30"
+                        rows="5"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    ></textarea>
+                    <button
+                        className="px-4 py-2 bg-indigo-600 text-white rounded-md ml-2 text-sm"
+                        onClick={handleComment}
+                    >
+                        Submit
+                    </button>
+                </div>
+                <p className="mt-4">
+                    <Link to="/">
+                        <i className="text-gray-600 text-sm">
+                            Log in if you want to comment
+                        </i>
+                    </Link>
+                </p>
+                {allComments.length > 0 ? (
+                    allComments.map((comment) => (
+                        <>
+                            <Comments comment={comment} key={comment._id}></Comments>
+                        </>
+
+                    ))
+                ) : (
+                    <p className="text-gray-600 text-sm">
+                        There are no comments for this product yet.
+                    </p>
+                )}
+                {/* Pagination */}
+                <div>
+                    <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+                        Précédent
+                    </button>
+                    <span> Page:  {currentPage} </span>
+                    <button onClick={() => handlePageChange(currentPage + 1)}>
+                        Suivant
+                    </button>
+                </div>
+            </div>
         </div>
-        <p className="mt-4">
-          <Link to="/">
-            <i className="text-gray-600 text-sm">
-              Log in if you want to comment
-            </i>
-          </Link>
-        </p>
-        {allComments.length > 0 ? (
-          allComments.map((comment) => (
-            <Comments comment={comment} key={comment._id}></Comments>
-          ))
-        ) : (
-          <p className="text-gray-600 text-sm">
-            There are no comments for this product yet.
-          </p>
-        )}
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDetail
--- server/controllers/Comments.js
+++ server/controllers/Comments.js
@@ -1,9 +1,9 @@
 const Comments = require('../models/comment-model')
 
 const createComment = async (req, res) => {
-    const {comment, id, userId} = req.body;
+    const { comment, id, userId } = req.body;
 
-    if(!comment) {
+    if (!comment) {
         res.status(400).json({ error: 'comment is required' });
     } else {
         Comments.create({
@@ -13,17 +13,33 @@ const createComment = async (req, res) => {
         });
         // const allComments = await fetchAllComments();
         // console.log(allProducts)
-        res.status(200).json({success: 'comment added successfully'});
+        res.status(200).json({ success: 'comment added successfully' });
     }
 }
 
+// const fetchAllComments = async (req, res) => {
+//     console.log("fetching all comments launched")
+//     try {
+//         comments = await Comments.find({});
+//         res.status(200).json(comments)
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
 const fetchAllComments = async (req, res) => {
-    console.log("fetching all comments launched")
+    const productId = req.query.productId;
+    const page = req.query.page || 1;
+    const perPage = 10; // Nombre de commentaires par page
+
     try {
-        comments = await Comments.find({});
-        res.status(200).json(comments)
+        const comments = await Comments.find({ productId })
+            .skip((page - 1) * perPage)
+            .limit(perPage);
+        res.json(comments);
     } catch (error) {
         console.error(error);
+        res.status(500).json({ error: 'Error while trying to fetch comments' });
     }
 }
 
